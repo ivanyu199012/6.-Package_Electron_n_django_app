42f35f2f35ee8ec2f32ce97f40f94715d026c15c
ivanyu199012 <chuenyuen027546@gmail.com>
2022-05-07 16:41:34 +0900

    DB 결로 수정

--- python/edtwExample/edtwExample/settings/dev.py
+++ python/edtwExample/edtwExample/settings/dev.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'edtwExample.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR.parent / 'db.sqlite3',
     }
 }
 
